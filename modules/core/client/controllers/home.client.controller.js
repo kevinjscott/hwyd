ae7fd24a9077eaddde8b01e994afcc3a725edc7d
Kevin J. Scott <kevinjscott@gmail.com>
2016-12-11 00:09:17 -0500

    home page demo links

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,18 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'moment',
+  function ($scope, Authentication, moment) {
+    var i, o;
+
     // This provides Authentication context.
     $scope.authentication = Authentication;
+
+    $scope.dates = [];
+    for (i = 0; i < 7; i++) {
+      o = {};
+      o.pretty = moment().add(i, 'days').format('ddd, MMM D');
+      o.ms = moment().add(i, 'days').format('x');
+      $scope.dates.push(o);
+    }
   }
 ]);
